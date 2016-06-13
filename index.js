f4f3ebfffc9095d037efd34e7f59c224594fa2ec
Venkat Kaniti <hvkaniti@gmail.com>
2016-06-13 00:12:28 -0400

    1. webpack.config.js is renamed to webpack.config.dev.js as to separte production and dev versions.

--- index.js
+++ index.js
@@ -4,9 +4,13 @@ var webpackDevMiddleware = require('webpack-dev-middleware');
 var webpackHotMiddleware = require('webpack-hot-middleware');
 var express = require('express');
 var app = express();
-
-var config = require('./webpack.config.js');
-var compiler = webpack(config); 
+var config;
+if (process.env.NODE_ENV === 'production') {
+	config = require('./webpack.config.prod.js');
+} else {
+	config = require('./webpack.config.dev.js');
+}
+var compiler = webpack(config);
 
 process.env.PORT = process.env.PORT || 3000;
 
@@ -16,10 +20,10 @@ app.use(new webpackDevMiddleware(compiler, {
 
 app.use(new webpackHotMiddleware(compiler));
 
-app.get('*', function(req, res){
+app.get('*', function (req, res) {
 	res.sendFile(path.resolve(__dirname, 'src/index.html'));
 });
 
-app.listen(process.env.PORT, function(){
+app.listen(process.env.PORT, function () {
 	console.log('application started at ' + process.env.PORT);
 });
